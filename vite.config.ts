cde7be5d6211162b8bb99032ac810333c9dbb1b1
Bhaskar <150775185+bhaskarsabbisetti@users.noreply.github.com>
2025-11-06 16:36:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     exclude: ['lucide-react'],
-//   },
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: './', // 👈 add this line
-})
-
+});
