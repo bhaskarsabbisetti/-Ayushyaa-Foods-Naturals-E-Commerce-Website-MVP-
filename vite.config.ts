47f5b1067cab1b6c51ae9016e6dc6598a75c59e8
bhaskarsabbisetti <bhaskarsabbisetti@gmail.com>
2025-11-06 16:14:03 +0530

    first

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+  base: './', // 👈 add this line
+})
+
